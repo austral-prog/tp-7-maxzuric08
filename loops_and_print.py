7977467bbd0dee199fe6b5da63102216889d78cc
maxzuric08 <162721708+maxzuric08@users.noreply.github.com>
2024-05-19 10:01:11 -0400

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,20 @@
 def enumerate_list(list):
-    return list
+    new_list=[]
+    a=0
+    for i in list:
+        if i!="":
+            new_indice=f'{a}. {i}'
+            a+=1
+            new_list.append(new_indice)
+    return new_list
 
 
 def enumerate_backwards(list):
-    return list
+    new_list=[]
+    a=0
+    for i in list:
+        if i!="":
+            new_indice=f'{a}. {i[-1::-1]}'
+            a+=1
+            new_list.append(new_indice)
+    return new_list
