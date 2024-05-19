fee6f56b60e46024fdf1332c4ab58ac63baa57f6
maxzuric08 <162721708+maxzuric08@users.noreply.github.com>
2024-05-19 10:01:46 -0400

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,50 @@
 def index_of_by_index(word, list, index):
-    return -1
+    for i in range(len(list)):
+        if i>=index:
+            if list[i]==word:
+                return i
+    new_list=list[index:]
+    if word not in new_list:
+        return -1    
 
 
 def index_of_empty(list):
-    return -1
+    for i in range(len(list)):
+        a=0
+        if list[i]=="" and a<1:
+            a+=1
+            return i
+    if "" not in list:
+        return -1
 
 
 def index_of(word, list):
-    return -1
+    for i in range(len(list)):
+        if list[i]==word:
+            return i
+    if word not in list:
+        return -1
+    
 
 
 def put(word, list):
-    return -1
-
+    for i in range(len(list)):
+        a=0
+        if list[i]=="" and a<1:
+            list[i]=word
+            a+=1
+            return i
+    if "" not in list:
+        return -1
+    
 
 def remove(word, list):
-    return -1
+    if word in list:
+        a=0
+        for i in range(len(list)):
+            if list[i]==word:
+                list[i]=""
+                a+=1
+        return a
+    else:
+        return 0
